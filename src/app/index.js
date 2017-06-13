2097b6fabb6c7a87bdbb441029838e76e8458fd8
shikha.gupta <shikha.gupta@quikr.com>
2017-06-13 09:48:56 +0530

    Setting up webpack server

---
+++ src/app/index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Shikha on 13/06/17.
+ */
+
+import React from "react";
+import {render} from "react-dom";
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+
+        };
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                    <h1> Hello </h1>
+            </div>
+    );
+    }
+}
+
+render(<App />, window.document.getElementById('app'));
